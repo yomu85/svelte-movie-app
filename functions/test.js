060e5e8262daf47434e60c8dd78194d079812705
yomu85 <yomu85@gmail.com>
2022-02-18 17:21:27 +0900

    Add Netlify functions

---
+++ functions/test.js
@@ -0,0 +1,10 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      name: 'yomu85',
+      age: 85,
+      email: 'yomu85@gmail.com'
+    })
+  }
+}
